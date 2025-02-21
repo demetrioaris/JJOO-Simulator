efbb0d88fe16eb9373225c01fddf9a43e2ca30e9
Demetrio Aris <gon21010@byui.edu>
2025-02-20 23:26:03 -0500

    change to HTML display

--- app/app.js
+++ app/app.js
@@ -2,13 +2,3 @@
 
 console.log("Welcome to JJOO Simulator");
 
-let userEntry;
-
-while (userEntry != 5) {
-    console.log("Choose one of the follow options");
-    console.log("1. Register of events");
-    console.log("2. Register of participants");
-    console.log("3. Simulate of events");
-    console.log("4. Create a report");
-    console.log("5. Exit of the program");
-}
